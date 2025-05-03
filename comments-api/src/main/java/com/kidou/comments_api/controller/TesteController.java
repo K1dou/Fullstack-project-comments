c0ab44c6ad5e9ae130eca6be48c80df9526c9fea
K1dou <hique1276@gmail.com>
2025-05-03 14:36:41 -0300

    created entity user

---
+++ comments-api/src/main/java/com/kidou/comments_api/controller/TesteController.java
@@ -0,0 +1,16 @@
+package com.kidou.comments_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TesteController {
+
+    @GetMapping("/teste")
+    public String teste() {
+        return "Hello World!";
+    }
+
+}
