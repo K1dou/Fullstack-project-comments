18bda03ec9f73b1709d5c99cc4800bedca2aa0ea
K1dou <hique1276@gmail.com>
2025-05-03 13:50:02 -0300

    first commit

---
+++ comments-api/src/test/java/com/kidou/comments_api/CommentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidou.comments_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
