f81530fdf0ecb7d2adcfed2514c0912edaeb55f2
K1dou <hique1276@gmail.com>
2025-05-05 22:30:38 -0300

    feat: add CORS configuration to allow requests from localhost:5173

---
+++ comments-api/src/main/java/com/kidou/comments_api/configuration/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.kidou.comments_api.configuration;
+
+@configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
