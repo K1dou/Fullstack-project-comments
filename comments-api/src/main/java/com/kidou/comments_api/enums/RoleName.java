10e0aa50433f4abf63e05e309afaa9af2853f205
K1dou <hique1276@gmail.com>
2025-05-06 14:11:39 -0300

    feat: implement user roles and custom user details service for enhanced security

---
+++ comments-api/src/main/java/com/kidou/comments_api/enums/RoleName.java
@@ -0,0 +1,7 @@
+package com.kidou.comments_api.enums;
+
+public enum RoleName {
+    ROLE_CUSTOMER,
+    ROLE_ADMIN
+
+}
--- comments-api/src/main/java/com/kidou/comments_api/model/User.java
+++ comments-api/src/main/java/com/kidou/comments_api/model/User.java
@@ -1,15 +1,27 @@
 package com.kidou.comments_api.model;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +38,10 @@ public class User {
 
     @Column(name = "avatar_url")
     private String avatarUrl;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
 
     public User() {
     }
@@ -75,6 +91,31 @@ public class User {
 
     public void setAvatarUrl(String avatarUrl) {
         this.avatarUrl = avatarUrl;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> (GrantedAuthority) () -> role.getName().name()).toList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
