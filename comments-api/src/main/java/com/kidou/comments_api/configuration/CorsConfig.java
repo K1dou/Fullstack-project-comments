8af368ddcb88e2690bb12bc0cffcddc7a2384e9d
K1dou <hique1276@gmail.com>
2025-05-07 11:05:22 -0300

    fix: update CORS configuration to use allowedOriginPatterns for better flexibility

--- comments-api/src/main/java/com/kidou/comments_api/configuration/CorsConfig.java
+++ comments-api/src/main/java/com/kidou/comments_api/configuration/CorsConfig.java
@@ -14,8 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("*")
+                        .allowedOriginPatterns("http://localhost:5173").allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
